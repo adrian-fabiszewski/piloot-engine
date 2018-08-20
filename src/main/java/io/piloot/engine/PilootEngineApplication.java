e7171e3280b99ad66ed046c748c15e508ec19a6f
Adrian Fabiszewski <adrian.fabiszewski@outlook.com>
2018-08-20 20:33:27 +0200

    Bootstrap backend application - spring starter

---
+++ src/main/java/io/piloot/engine/PilootEngineApplication.java
@@ -0,0 +1,12 @@
+package io.piloot.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilootEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilootEngineApplication.class, args);
+	}
+}
