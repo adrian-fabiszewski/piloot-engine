bac72a1055737fa76f72792bbafe9a14e954cbd6
Adrian Fabiszewski <adrian.fabiszewski@outlook.com>
2018-08-20 20:46:34 +0200

    Bootstrap backend application - checkstyle added

--- src/main/java/io/piloot/engine/PilootEngineApplication.java
+++ src/main/java/io/piloot/engine/PilootEngineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PilootEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PilootEngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PilootEngineApplication.class, args);
+    }
 }
